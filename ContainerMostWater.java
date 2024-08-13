096c0e615fde7149cf2983bd510eb7bd4ac50c8f
Aditya Rai <51072067+arai7@users.noreply.github.com>
2024-08-13 11:18:11 +0530

    Create ContainerMostWater.java

---
+++ ContainerMostWater.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            // Calculate the area
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+
+            // Update the maximum area found so far
+            maxArea = Math.max(maxArea, currentArea);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
