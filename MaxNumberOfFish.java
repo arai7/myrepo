a16c1cd2cb482cac915898660c488835dec73442
Aditya Rai <51072067+arai7@users.noreply.github.com>
2025-01-28 19:24:50 +0530

    Create MaxNumberOfFish.java

---
+++ MaxNumberOfFish.java
@@ -0,0 +1,40 @@
+class Solution 
+{
+    int rows = 0;
+    int cols = 0;
+
+    public int findMaxFish(int[][] grid) 
+    {
+        if(grid == null || grid.length==0)
+            return 0;
+
+        rows = grid.length;
+        cols = grid[0].length;
+        
+        int maxArea = 0;
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(grid[i][j] > 0)
+                {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(area, maxArea);
+                }
+            }
+        }
+        return maxArea;   
+    }
+
+    public int dfs(int[][] grid, int i, int j)
+    {
+        if(i >= rows || i < 0 || j < 0 || j >= cols || grid[i][j]==0)
+            return 0;
+
+        int fishCount = grid[i][j];
+        grid[i][j] = 0;
+
+        return fishCount+dfs(grid, i+1, j) + dfs(grid, i-1, j) + dfs(grid, i, j+1) + dfs(grid, i, j-1);
+    }
+}
