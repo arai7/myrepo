52d2a1a65b75d6a50a7c462ea733ac302ee260cb
Aditya Rai <51072067+arai7@users.noreply.github.com>
2025-05-02 12:44:19 +0530

    Update CoinChange.java

--- CoinChange.java
+++ CoinChange.java
@@ -1,18 +1,33 @@
 class Solution {
-    
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1); // Fill the dp array with a large value (amount + 1)
-        dp[0] = 0;
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        int result = backtrack(coins, amount, memo);
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
 
-        for (int i = 1; i <= amount; i++) {
-            for (int coin : coins) {
-                if (i - coin >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-                }
-            }
+    public int backtrack(int[] coins, int rem, HashMap<Integer, Integer> memo){
+        if(rem < 0){
+            return Integer.MAX_VALUE;
         }
 
-        return dp[amount] > amount ? -1 : dp[amount];
+        if(rem == 0){
+            return 0;
+        }
+
+        if(memo.containsKey(rem)){
+            return memo.get(rem);
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int coin : coins){
+            
+            int res = backtrack(coins, rem - coin, memo);
+            if(res != Integer.MAX_VALUE)
+                min = Math.min(min, res+1);
+        }
+
+        memo.put(rem, min);
+        return min;
     }
 }
